5fe60cdc9cad0a6c41b9bdf96b0050ccb8a21aea
Brandon Yang <jqm9ba@virginia.edu>
2024-06-12 12:44:07 -0400

    init 3dgs react app

---
+++ feature-3dgs-react/src/App.tsx
@@ -0,0 +1,15 @@
+import renderer from "./utils/render"
+
+
+function App() {
+  renderer.render()
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>3DGS React</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
